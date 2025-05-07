3c6d37ffbe64a3870129482bce52360b809bb59d
artem <cheremnykh1707@mail.ru>
2025-05-07 10:31:35 +0500

    Добавьте файлы проекта.

---
+++ Diplom_Cheremnykh/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Diplom_Cheremnykh
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
